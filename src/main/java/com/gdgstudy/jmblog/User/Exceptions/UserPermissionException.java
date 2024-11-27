8d9222e911dbaa3b21332790e56a2f005c8e79e4
xjmx315 <xjmx315@gmail.com>
2024-11-27 18:11:54 +0900

    [feat] 튀어나오는 모든 에러 처리

--- src/main/java/com/gdgstudy/jmblog/User/Exceptions/UserPermissionException.java
+++ src/main/java/com/gdgstudy/jmblog/User/Exceptions/UserPermissionException.java
@@ -3,5 +3,6 @@ package com.gdgstudy.jmblog.User.Exceptions;
 public class UserPermissionException extends RuntimeException {
     public UserPermissionException() {
         super("You Have Not Permission For");
+        //비밀번호가 다른 경우
     }
 }
